14f74c51a66dff889dbf0f3f83fd18fcb0b2bba2
Vijayavedhasekarn <vedhatech02@gmail.com>
2023-10-08 22:39:14 +0530

    adding header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins'],
+        'inter': ['Inter'],
+      },
+      colors: {
+        'cdn-red': '#F45050',
+        'cdn-yellow': '#F9D949',
+        'hero-card-color': '#D9D9D9',
+      }
+    },
   },
   plugins: [],
 }
